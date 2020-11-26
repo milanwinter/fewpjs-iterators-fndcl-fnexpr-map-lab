ffec717a2cd5fc373a98c4296e9533342bc7eb33
milanwinter <milanwinter8@gmail.com>
2020-11-25 17:05:23 -0800

    commit tree 610dea16f2dc9a65f74fb69da8a010f2f4cf2d62

--- index.js
+++ index.js
@@ -12,5 +12,25 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  return tutorials.map(tutorial => {
+
+    const words = tutorial.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+
+   
+    return words.join(" ")
+ })
 }
+
+
+// const words = mySentence.split(" ");
+
+// for (let i = 0; i < words.length; i++) {
+//     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+// }
+
+// words.join(" ");
